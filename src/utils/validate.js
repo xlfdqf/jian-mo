fb87865e2a0df04c6189a89a10a6568f162b7829
xulanfang <1574425015@qq.com>
2019-08-01 14:22:43 +0800

    测试添加分支

--- src/utils/validate.js
+++ src/utils/validate.js
@@ -2,7 +2,8 @@
  * Created by jiachenpan on 16/11/18.
  */
 
-// export function isvalidUsername(str) {
+// 验证用户名
+//export function isvalidUsername(str) {
 //   const valid_map = ['admin', 'editor']
 //   return valid_map.indexOf(str.trim()) >= 0
 // }

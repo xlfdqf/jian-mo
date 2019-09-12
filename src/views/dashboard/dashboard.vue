acf76bf8156485ebe96a114850b5e3efc3459e2b
xulanfang <1574425015@qq.com>
2019-09-12 16:15:28 +0800

    编写首页页面

--- src/views/dashboard/dashboard.vue
+++ src/views/dashboard/dashboard.vue
@@ -1,183 +1,229 @@
 <template>
-  <div class="app-container">
+  <div class="table">
     <el-card>
-      <div class="item">
-        <h4>数据展示</h4>
-        <nx-data-display :option="option"></nx-data-display>
-      </div>
-      <div class="item">
-        <h4>选项卡展示</h4>
-        <nx-data-tabs :option="easyDataOption"></nx-data-tabs>
-      </div>
-      <div class="item">
-        <h4>带数字的展示</h4>
-        <nx-data-icons :option="easyDataOption1"></nx-data-icons>
-      </div>
-      <div class="item">
-        <h4>简易展示</h4>
-        <nx-data-icons :option="easyDataOption2"></nx-data-icons>
-      </div>
+      <el-row>
+        <el-col :span="9" style="height:400px">
+          <div class="item">
+            <p>历史用户统计</p>
+            <el-col :span="8" class="item-content">
+              <div class="circle">
+                <div class="big"></div>
+                <div class="small"></div>
+                   <p>好人样本</p>
+                   <p style="top:65px;">23500</p>
+              </div>
+            </el-col>
+            <el-col :span="8" class="item-content">
+              <div class="circle">
+                <div class="big"></div>
+                <div class="small" style=" border: 10px solid #FF9745; border-bottom-color: transparent;"></div>
+                <p style="color:#FF9745;">灰名单样本</p>
+                <p style="color:#FF9745;top:65px;">2350</p>
+              </div>
+            </el-col>
+            <el-col :span="8" class="item-content">
+              <div class="circle">
+                <div class="big"></div>
+                <div class="small" style=" border: 10px solid #A859FF; border-bottom-color: transparent;"></div>
+                <p style="color:#A859FF;">黑户样本</p>
+                 <p style="color:#A859FF;top:65px;">235</p>
+              </div>
+            </el-col>
+          </div>
+          </el-col>
+        <el-col :span="6">
+          <div class="item">
+            <p>当前最重要特征iv值</p>
+             <el-col :span="24"><p>缴纳保险月份数：<span>0.58</span></p></el-col>
+             <el-col :span="24"><p>缴纳保险月份数：<span>0.58</span></p></el-col>
+            <el-col :span="24"><p>缴纳保险月份数：<span>0.58</span></p></el-col>
+            <el-col :span="24"><p>缴纳保险月份数：<span>0.58</span></p></el-col>
+            <el-col :span="24"><p>缴纳保险月份数：<span>0.58</span></p></el-col>
+          </div>
+          </el-col>
+        <el-col :span="9">
+            <div class="item">
+             <p>昨日样本分布 </p>
+              <el-col :span="8" class="water">
+               <p style="color:#40B1F9">好用户</p>
+                <div class="container">
+                  <div class="wave">
+                    <span>50%</span>
+                    </div>
+                </div>
+              </el-col>
+              <el-col :span="8" class="water">
+                <p style="color:#FF9745">灰名单</p>
+                <div class="container" style="border: 5px solid #FF9745;">
+                  <div class="wave" style="background-color: #F8CFAF;">
+                    <span style="color:#FF9745">50%</span>
+                    </div>
+                </div>
+              </el-col>
+              <el-col :span="8" class="water">
+                <p style="color:#A859FF">黑名单</p>
+                <div class="container" style="border: 5px solid #A859FF;">
+                  <div class="wave" style="background-color: #DCBFFC;">
+                    <span style="color:#A859FF">50%</span>
+                    </div>
+                </div>
+              </el-col>
+            </div>
+        </el-col>
+      </el-row>
     </el-card>
   </div>
 </template>
 
 <script>
-import nxDataDisplay from "@/components/nx-data-display/nx-data-display";
-import nxDataTabs from "@/components/nx-data-tabs/nx-data-tabs";
-import nxDataIcons from "@/components/nx-data-icons/nx-data-icons";
 export default {
   name: "report",
-  components: {
-    nxDataDisplay,
-    nxDataTabs,
-    nxDataIcons
-  },
   data() {
-    return {
-      option: {
-        span: 8,
-        color: "#15A0FF",
-        data: [
-          {
-            count: 1000,
-            title: "日活跃数"
-          },
-          {
-            count: 3000,
-            title: "月活跃数"
-          },
-          {
-            count: 20000,
-            title: "年活跃数"
-          }
-        ]
-      },
-      easyDataOption: {
-        span: 6,
-        data: [
-          {
-            title: "分类统计",
-            subtitle: "实时",
-            count: 7993,
-            allcount: 10222,
-            text: "当前分类总记录数",
-            color: "rgb(49, 180, 141)",
-            key: "类"
-          },
-          {
-            title: "附件统计",
-            subtitle: "实时",
-            count: 3112,
-            allcount: 10222,
-            text: "当前上传的附件数",
-            color: "rgb(56, 161, 242)",
-            key: "附"
-          },
-          {
-            title: "文章统计",
-            subtitle: "实时",
-            count: 908,
-            allcount: 10222,
-            text: "评论次数",
-            color: "rgb(117, 56, 199)",
-            key: "评"
-          },
-          {
-            title: "新闻统计",
-            subtitle: "实时",
-            count: 908,
-            allcount: 10222,
-            text: "评论次数",
-            color: "rgb(59, 103, 164)",
-            key: "新"
-          }
-        ]
-      },
-      easyDataOption1: {
-        color: "rgb(63, 161, 255)",
-        span: 4,
-        data: [
-          {
-            title: "今日注册",
-            count: 12678,
-            icon: "icon-cuowu"
-          },
-          {
-            title: "今日登录",
-            count: 22139,
-            icon: "icon-shujuzhanshi2"
-          },
-          {
-            title: "今日订阅",
-            count: 35623,
-            icon: "icon-jiaoseguanli"
-          },
-          {
-            title: "今日评论",
-            count: 16826,
-            icon: "icon-caidanguanli"
-          },
-          {
-            title: "今日评论",
-            count: 16826,
-            icon: "icon-caidanguanli"
-          },
-          {
-            title: "今日评论",
-            count: 16826,
-            icon: "icon-caidanguanli"
-          }
-        ]
-      },
-      easyDataOption2: {
-        color: "rgb(63, 161, 255)",
-        span: 4,
-        discount: true,
-        data: [
-          {
-            title: "错误日志",
-            count: 12678,
-            icon: "icon-cuowu"
-          },
-          {
-            title: "数据展示",
-            count: 12678,
-            icon: "icon-shujuzhanshi2"
-          },
-          {
-            title: "权限管理",
-            count: 12678,
-            icon: "icon-jiaoseguanli"
-          },
-          {
-            title: "菜单管理",
-            count: 12678,
-            icon: "icon-caidanguanli"
-          },
-          {
-            title: "权限测试",
-            count: 12678,
-            icon: "icon-caidanguanli"
-          },
-          {
-            title: "错误页面",
-            count: 12678,
-            icon: "icon-caidanguanli"
-          }
-        ]
-      }
-    };
+    return {};
   },
   created() {},
-  watch: {},
-  mounted() {},
   computed: {}
 };
 </script>
 
 <style scoped>
+.table >>> .el-card {
+  background-color: #091938;
+  border: none;
+}
+
 .item {
+  height: 400px;
   margin-bottom: 16px;
+  border: 1px solid #091938;
+  background-color: #080920;
+  margin-right: 20px;
+}
+.item p {
+  text-align: center;
+  color: #3fbde0;
+  font-weight: bold;
+}
+.item::after {
+  content: "";
+  display: block;
+  clear: both;
+}
+.item-content {
+  /* border: 1px solid red; */
+}
+/* 镂空圆环效果 */
+.circle {
+  position: relative;
+  box-sizing: border-box;
+  width: 10rem;
+  height: 10rem;
+  margin: 0 auto;
+}
+.big {
+  width: 10rem;
+  height: 10rem;
+  border-radius: 50%;
+  position: absolute;
+  margin: auto;
+  /*以下五个属性为水平垂直居中的方法*/
+  left: 0;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  box-sizing: border-box;
+}
+.small {
+  width: 8rem;
+  height: 8rem;
+  border: 10px solid #40b1f9;
+  border-radius: 50%;
+  position: absolute;
+  margin: auto;
+  /*以下五个属性为水平垂直居中的方法*/
+  left: 0;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  box-sizing: border-box;
+  border-bottom-color: transparent;
+}
+.circle p {
+  width: 90px;
+  height: auto;
+  position: absolute;
+  top: 45px;
+  left: 37px;
+  color: #40b1f9;
+  font-weight: bold;
+  text-align: center;
+}
+/* 水波纹效果 */
+.water {
+  position: relative;
+}
+/* .water p {
+  position: absolute;
+  left: 15px;
+} */
+.container {
+  position: absolute;
+  width: 100px;
+  height: 100px;
+  padding: 5px;
+  border: 5px solid #34abff;
+  top: 100px;
+  left: 100px;
+  transform: translate(-50%, -50%);
+  border-radius: 50%;
+  overflow: hidden;
+}
+.wave {
+  position: relative;
+  left: -10px;
+  width: 100px;
+  height: 100px;
+  background-color: #9dd0f0;
+  border-radius: 50%;
+  /* opacity: 0.7; */
+}
+.wave::before,
+.wave::after {
+  content: "";
+  position: absolute;
+  width: 210px;
+  height: 210px;
+  top: -15px;
+  left: 50%;
+  background-color: #080920;
+  border-radius: 45%;
+  transform: translate(-50%, -70%) rotate(0);
+  animation: rotate 4s linear infinite;
+  z-index: 10;
+}
+.wave::after {
+  border-radius: 47%;
+  background-color: #080920;
+  transform: translate(-50%, -70%) rotate(0);
+  animation: rotate 10s linear -5s infinite;
+  z-index: 20;
+}
+@keyframes rotate {
+  50% {
+    transform: translate(-50%, -73%) rotate(180deg);
+  }
+  100% {
+    transform: translate(-50%, -70%) rotate(360deg);
+  }
+}
+.wave span {
+  color: #40b1f9;
+  font-weight: bold;
+  font-size: 25px;
+  z-index: 999;
+  position: absolute;
+  left: 25px;
+  top: 25px;
 }
 </style>
 <style lang ="scss">
